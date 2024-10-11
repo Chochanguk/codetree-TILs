bc5013d914c8c0b671c2bfd76f31bcecb9378c28
조창욱 <119058637+Chochanguk@users.noreply.github.com>
2024-10-11 11:51:35 +0900

    Title: 코드트리 투어, Time: 1906ms, Memory: 92MB, Status: Passed - CodeTree

--- 241011/코드트리 투어/codetree-tour.py
+++ 241011/코드트리 투어/codetree-tour.py
@@ -1,85 +1,118 @@
 import heapq
+import sys
 
-# 다익스트라 알고리즘을 사용해 start에서 모든 목표 노드까지의 최단 거리를 계산
-def dijkstra_all_targets(start, n, graph):
-    dist = [float('inf')] * n
-    dist[start] = 0
-    q = [(0, start)]  # (거리, 노드)
+INF = float('inf')  # 무한대 값을 정의합니다.
+MAX_N = 2000  # 코드트리 랜드의 최대 도시 개수입니다.
+MAX_ID = 30005  # 여행상품 ID의 최대값입니다.
 
-    while q:
-        current_dist, current_node = heapq.heappop(q)
+# 입력을 빠르게 받기 위한 설정입니다.
+input = sys.stdin.readline
 
-        if current_dist > dist[current_node]:
+N, M = 0, 0  # 도시의 개수 N과 간선의 개수 M을 초기화합니다.
+A = []  # 코드트리 랜드의 간선을 인접 행렬로 저장합니다.
+D = []  # 다익스트라 알고리즘을 통해 시작도시부터 각 도시까지의 최단경로를 저장합니다.
+isMade = []  # 여행상품이 만들어진적 있는지 저장합니다.
+isCancel = []  # 여행상품이 취소되었는지 저장합니다.
+S = 0  # 여행 상품의 출발지입니다.
+
+class Package:
+    def __init__(self, id, revenue, dest, profit):
+        self.id = id
+        self.revenue = revenue
+        self.dest = dest
+        self.profit = profit
+
+    def __lt__(self, other):
+        if self.profit == other.profit:
+            return self.id < other.id
+        return self.profit > other.profit
+
+pq = []  # 우선순위 큐 초기화
+
+# dijkstra 알고리즘을 heapq를 이용해 개선한 함수입니다.
+def dijkstra():
+    global D
+    D = [INF] * N
+    D[S] = 0
+    pq = [(0, S)]  # (거리, 도시) 형식으로 우선순위 큐에 시작 도시를 넣음
+
+    while pq:
+        current_dist, v = heapq.heappop(pq)
+
+        # 이미 처리된 도시라면 건너뜁니다.
+        if current_dist > D[v]:
             continue
 
-        for next_node, weight in graph.get(current_node, []):
-            distance = current_dist + weight
+        # v 도시와 인접한 모든 도시를 확인하여 최단 거리 갱신
+        for u in range(N):
+            if A[v][u] != INF:  # 연결된 도시인 경우
+                next_dist = current_dist + A[v][u]
+                if next_dist < D[u]:
+                    D[u] = next_dist
+                    heapq.heappush(pq, (next_dist, u))
 
-            if distance < dist[next_node]:
-                dist[next_node] = distance
-                heapq.heappush(q, (distance, next_node))
+# 코드트리랜드를 입력받고 주어진 코드트리 랜드를 인접행렬에 저장합니다
+def buildLand(n, m, arr):
+    global A, N, M
+    N, M = n, m
+    A = [[INF] * N for _ in range(N)]
+    for i in range(N):
+        A[i][i] = 0  # 도시 자신에게 가는 비용은 0입니다.
+    for i in range(M):
+        u, v, w = arr[i * 3], arr[i * 3 + 1], arr[i * 3 + 2]
+        A[u][v] = min(A[u][v], w)
+        A[v][u] = min(A[v][u], w)
 
-    return dist
+# 여행 상품을 추가합니다
+def addPackage(id, revenue, dest):
+    isMade[id] = True
+    profit = revenue - D[dest]
+    heapq.heappush(pq, Package(id, revenue, dest, profit))
 
+# id에 해당하는 여행상품이 취소되었음을 기록합니다
+def cancelPackage(id):
+    if isMade[id]:
+        isCancel[id] = True
 
-# 메인 함수 정의와 실행
-Q = int(input())  # 명령 수
-graph = {}  # 도시에 대한 그래프
-products = {}  # 여행 상품 정보를 저장
-start_city = 0  # 초기 출발 도시
-n = 0  # 도시의 개수를 저장할 변수
-dist = []  # 출발지로부터 모든 도시까지의 최단 거리 리스트
+# 최적의 여행상품을 판매합니다
+def sellPackage():
+    while pq:
+        p = pq[0]
+        if p.profit < 0:
+            break
+        heapq.heappop(pq)
+        if not isCancel[p.id]:
+            return p.id
+    return -1
 
+# 변경할 시작도시를 입력받고 변경됨에 따른 기존 여행상품 정보들을 수정합니다.
+def changeStart(param):
+    global S
+    S = param
+    dijkstra()  # 새로운 출발지에 대해 다익스트라 알고리즘을 다시 실행합니다.
+    temp_packages = []
+    while pq:
+        temp_packages.append(heapq.heappop(pq))
+    for p in temp_packages:
+        addPackage(p.id, p.revenue, p.dest)
+
+Q = int(input())
+isMade = [False] * MAX_ID
+isCancel = [False] * MAX_ID
 for _ in range(Q):
-    command = list(map(int, input().split()))
-    cmd_type = command[0]
+    query = list(map(int, input().split()))
+    T = query[0]
 
-    if cmd_type == 100:  # 코드트리 랜드 건설
-        n, m = command[1], command[2]
-        graph = {i: [] for i in range(n)}  # 그래프 초기화
-        for i in range(m):
-            v, u, w = command[3 + 3 * i], command[4 + 3 * i], command[5 + 3 * i]
-            graph[v].append((u, w))
-            graph[u].append((v, w))
-
-        # 최초 출발지에서 모든 도시까지의 최단 거리 계산
-        dist = dijkstra_all_targets(start_city, n, graph)
-
-    elif cmd_type == 200:  # 여행 상품 생성
-        id, revenue, dest = command[1], command[2], command[3]
-        # 상품의 목표지점까지 최단 거리 저장 (이미 계산된 dist 리스트 활용)
-        cost = dist[dest]
-        products[id] = (revenue, dest, cost)
-
-    elif cmd_type == 300:  # 여행 상품 취소
-        id = command[1]
-        if id in products:
-            del products[id]
-
-    elif cmd_type == 400:  # 최적의 여행 상품 판매
-        best_product = None
-        best_value = -float('inf')
-
-        for id, (revenue, dest, cost) in products.items():
-            profit = revenue - cost  # 이득
-
-            if profit >= 0 and (best_product is None or profit > best_value or (profit == best_value and id < best_product)):
-                best_product = id
-                best_value = profit
-
-        if best_product is not None:
-            print(best_product)
-            del products[best_product]
-        else:
-            print(-1)
-
-    elif cmd_type == 500:  # 여행 상품의 출발지 변경
-        start_city = command[1]
-        # 새로운 출발지에서 모든 도시까지의 최단 거리 계산
-        dist = dijkstra_all_targets(start_city, n, graph)
-
-        # 모든 상품의 cost 값을 새 출발지에서의 최단 거리로 갱신
-        for id in products:
-            revenue, dest, _ = products[id]
-            new_cost = dist[dest]
-            products[id] = (revenue, dest, new_cost)
+    if T == 100:
+        buildLand(query[1], query[2], query[3:])
+        dijkstra()
+    elif T == 200:
+        id, revenue, dest = query[1], query[2], query[3]
+        addPackage(id, revenue, dest)
+    elif T == 300:
+        id = query[1]
+        cancelPackage(id)
+    elif T == 400:
+        print(sellPackage())
+    elif T == 500:
+        changeStart(query[1])
